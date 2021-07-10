a45d90c663fada93736599af179787028095e780
Jackgoers96 <81663225+Jackgoers96@users.noreply.github.com>
2021-07-09 21:04:48 -0500

    Timer works, as well as clearing. Setting the groundwork for the options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,42 @@
-var time = document.querySelector("#time")
+//set timer
+const gameTimeEl = document.getElementById("time");
+let startButtonEl = document.getElementById("start")
+let gg = document.getElementById("gg")
+var myVar;
 
-var timeInt= parseInt(time, 10);
 
+function startTimer() {
+    startButtonEl.remove()
+    myVar = setInterval(countdownTime, 1000);
+}
+startButtonEl.addEventListener("click", startTimer)
 
+function countdownTime() {
+    
+    gameTimeEl.innerHTML -= 1; 
 
-function timerColor() {
-    if (timeInt <= 15) {
-    timeInt.fontcolor("red");
-    }
-    return
+    if (gameTimeEl.innerHTML == 0) {
+        clearInterval(myVar);
+        displayEndGameMessage(); 
+        
+    };
 }
 
-    //This var holds the entire questions array and can be called by it's sections
+
+
+
+function displayEndGameMessage() {
+    if (gameTimeEl.innerHTML <= 0 ) {
+    gg.remove();
+    }
+}
+    // else if (gameTimeEl > 0 you win)
+
+
+
+
+
+    // This var holds the entire questions array and can be called by it's sections
     var currentQuestion = quiz[currentIndex];
     var questionsH2 = document.createElement('h2');
     questionsH2.textContent = currentQuestion.title;
@@ -33,3 +58,40 @@ function timerColor() {
     console.log("choicesBtn", choicesBtn);
     choicesBtn.forEach(element => {
     });
+
+
+
+
+
+    var quiz = [
+        {
+            title: "Who famously fell from Nakatomi Plaza on Christmas Eve 1988?",
+            choices: ["Vizzini", "Hans Gruber", "Norman Bates", "Cyberdyne Model 101 Terminator"],
+            answer: "Hans Gruber"
+        },
+    
+        {
+            title: "Who once declared that the town needed an enema?",
+            choices: ["Hannibal Lecter", "Annie Wilkes", "Joker", "Jack Torrance"],
+            answer: "Joker"
+        },
+    
+        {
+            title: "Who famously declared they would never hurt a fly?",
+            choices: ["Nurse Ratched", "Alex DeLarge", "Baby Jane", "Norman Bates"],
+            answer: "Norman Bates"
+        },
+    
+        {
+            title: "Who once hobbled Paul Sheldon while firmly swearing to be his number one fan?",
+            choices: ["Talia Al Ghul", "Annie Wilkes", "Bellatrix Lestrange", "O-Ren Ishii"],
+            answer: "Annie Wilkes"
+        },
+    
+        {
+            title: "Who proved they could lie still for a very long time in their debut movie?",
+            choices:["John Kramer", "Catherine Tramell", "Jason Voorhees", "Talia Al Ghul"],
+            answer: "John Kramer"
+        }
+    
+    ]
